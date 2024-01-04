1afe6fe248c0e95edb32b73e8e4dbbc1c3c53f64
Jovany-java <142845569+Jovany-java@users.noreply.github.com>
2024-01-04 15:40:55 -0600

    Update Jovany.js

--- Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/javascript/Jovany.js
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/javascript/Jovany.js
@@ -1,14 +1,34 @@
 /**EJERCICIO:
  * - Crea un comentario en el código y coloca la URL del sitio web oficial del lenguaje de programación que has seleccionado.
-      Sitio web oficial de Java Script:    https://developer.mozilla.org/es/docs/Web/JavaScript
+      a-. Sitio web oficial de Java Script: https://developer.mozilla.org/es/docs/Web/JavaScript
+
+ * - Representa las diferentes sintaxis que existen de crear comentarios en el lenguaje (en una línea, varias...).
+      a.- //Soy un comentario de una linea
+      b.- /**
+
+            Soy un comentario multilinea
+     
+      **/
 
 
- * - Representa las diferentes sintaxis que existen de crear comentarios
- *   en el lenguaje (en una línea, varias...).
- * - Crea una variable (y una constante si el lenguaje lo soporta).
- * - Crea variables representando todos los tipos de datos primitivos
+  /**      
+  
+   **/
+
+// * - Crea una variable (y una constante si el lenguaje lo soporta).
+      let MyVariable =0;
+      const Myconstan =0;
+      
+ //* - Crea variables representando todos los tipos de datos primitivos
+
+MyBolean = true;
+
+/** 
+
+ 
  *   del lenguaje (cadenas de texto, enteros, booleanos...).
  * - Imprime por terminal el texto: "¡Hola, [y el nombre de tu lenguaje]!"
 
  
 **/
+
